b52f2efcb3bc5b62851857503300e67c4090fe39
Casey Leask <caseyleask@gmail.com>
2013-11-04 23:36:20 +1100

    Compacted the console log calls

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,23 +6,20 @@ define(['variadic'], function (variadic) {
         return first;
     }
 
-    console.log(unary('why', 'hello', 'there'));
+    console.log(unary('why', 'hello', 'there'),
+                variadic(unary)('why', 'hello', 'there'));
     //=> 'why'
-
-    console.log(variadic(unary)('why', 'hello', 'there'));
     //=> [ 'why', 'hello', 'there' ]
 
     function binary (first, rest) {
         return [first, rest];
     }
 
-    console.log(binary('why', 'hello', 'there'));
+    console.log(binary('why', 'hello', 'there'),
+                variadic(binary)('why', 'hello', 'there'),
+                variadic(binary)('why'));
     //=> [ 'why', 'hello' ]
-
-    console.log(variadic(binary)('why', 'hello', 'there'));
     //=> [ 'why', [ 'hello', 'there' ] ]
-
-    console.log(variadic(binary)('why'));
     //=> [ 'why', [ ] ]
 
     return 'App is finished!';
